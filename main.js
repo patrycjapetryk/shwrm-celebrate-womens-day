9c4aa5cb72be94f7b3d1eef52ad3264371862db8
Patrycja Petryk <51820512+patrycjapetryk@users.noreply.github.com>
2024-02-12 13:51:12 +0100

    Add content

--- main.js
+++ main.js
@@ -1,24 +1,10 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+window.addEventListener('load', () => {
+  const lastArticle = document.querySelector('.page__article:nth-last-of-type(1)');
 
-setupCounter(document.querySelector('#counter'))
+  window.onscroll = () =>
+    window.pageYOffset < 200 || window.pageYOffset > lastArticle.offsetTop
+      ? (document.querySelector('.page__background').style.backgroundColor = '#ffffff')
+      : (document.querySelector('.page__background').style.backgroundColor = '#fce1d8');
+});
